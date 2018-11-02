518b4deb64cf8bdd48e5a3af906bf3fbe28b44fa
CingyQ <cingyq@qq.com>
2018-11-02 21:41:32 +0800

    重写了PrintDirectClass

--- Printer_Form/Printer_Form/Form1.Designer.cs
+++ Printer_Form/Printer_Form/Form1.Designer.cs
@@ -30,19 +30,18 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(495, 161);
+            this.button1.Location = new System.Drawing.Point(802, 277);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(185, 98);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "打印";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -54,35 +53,24 @@
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // printDocument1
-            // 
-            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
-            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(82, 135);
+            this.pictureBox1.Location = new System.Drawing.Point(82, 114);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(234, 218);
+            this.pictureBox1.Size = new System.Drawing.Size(511, 443);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // button2
+            // printDocument1
             // 
-            this.button2.Location = new System.Drawing.Point(507, 296);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1156, 616);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -99,9 +87,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Drawing.Printing.PrintDocument printDocument1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Drawing.Printing.PrintDocument printDocument1;
     }
 }
 
--- Printer_Form/Printer_Form/Form1.cs
+++ Printer_Form/Printer_Form/Form1.cs
@@ -29,42 +29,59 @@ namespace Printer_Form
         int temp = 1;
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
             String A, B, C;
             String AbsoluteSave = "d:\\img";
-            A = textBox1.Text;
-            C = StaticClass.StringEncoder(A);
-            B = StaticClass.StringDecder(C);
-            QRC.CreateQRCode(AbsoluteSave, C, temp);
-            PrintDirectClass p = new PrintDirectClass();
-            p.PrintPreview();
-            textBox1.Text = "";
-
+            
+            
+                A = textBox1.Text;
+                C = StaticClass.StringEncoder(A);
+                B = StaticClass.StringDecder(C);
+                QRC.CreateQRCode(AbsoluteSave, C, temp,8);
+                //Console.WriteLine(A);
+                Console.WriteLine(C);
+                Console.WriteLine(B);
+            pictureBox1.ImageLocation = "d:\\img\\1.png";
+            printDocument1.Print();
 
 
         }
 
-        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+       
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
             
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-           
-        }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            String A, B, C;
-            String AbsoluteSave = "d:\\img";
-            A = textBox1.Text;
-            C = StaticClass.StringEncoder(A);
-            B = StaticClass.StringDecder(C);
-            QRC.CreateQRCode(AbsoluteSave, C, temp);
-            PrintDirectClass p = new PrintDirectClass();
-            p.PrintPreview();
-            textBox1.Text = "";
+            printDocument1.Print();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            //e.Graphics.DrawImage(pictureBox1.Image, 20, 20);
+            int x = e.MarginBounds.X;
+            int y = e.MarginBounds.Y;
+            int width = pictureBox1.Image.Width;
+            int height = pictureBox1.Image.Height;
+            if ((width / e.MarginBounds.Width) > (height / e.MarginBounds.Height))
+            {
+                width = e.MarginBounds.Width;
+                height = pictureBox1.Image.Height * e.MarginBounds.Width / pictureBox1.Image.Width;
+            }
+            else
+            {
+                height = e.MarginBounds.Height;
+                width = pictureBox1.Image.Width * e.MarginBounds.Height / pictureBox1.Image.Height;
+            }
+            int big = 300;
+            int a = e.PageBounds.Width / 2 - (pictureBox1.Image.Width +big) / 2;
+            int b = e.PageBounds.Height / 2 - (pictureBox1.Image.Height + big) / 2;
+            e.Graphics.DrawImage(pictureBox1.Image, a, b, pictureBox1.Image.Width + big, pictureBox1.Image.Height + big);
+            
+
+            
         }
     }
 }
